b9e3b7c1ba204348f0e6383e0acc200fa23b1afa
saketh192 <sakethraman964@gmail.com>
2025-11-08 21:45:13 +0530

    python lecture 2

---
+++ python.py/lectuure-2.py
@@ -0,0 +1,18 @@
+name = "Saketh"
+age = 14
+weight = 60
+is_student = True
+print(name)
+print("The type of name is :" , type(name))
+print(age)
+print("The type of age is :" , type(age))
+print(weight)
+print("The type of weight is :" , type(weight))
+print(is_student)
+print("The type of is_student is :" , type(is_student))
+
+age = str(age)
+print( "The type of age is :" , type(age))
+
+weight = int(weight)
+print("The type of weight is :" , type(weight))
