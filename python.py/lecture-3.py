60f07044b63b2607735425f204e302863cea6c39
saketh192 <sakethraman964@gmail.com>
2025-11-14 21:35:55 +0530

    lecture-3

---
+++ python.py/lecture-3.py
@@ -0,0 +1,40 @@
+# Activity-1
+number = int(input("Enter a number"))
+print("Number is :",  number)
+if number%2==0:
+    print("Number is even")
+else:
+    print("Number is odd")
+
+    # activity-2
+weight = int(input("Enter your weight"))
+height = int(input("Enter your height"))
+BMI = weight/(height/100)**2
+print("your BMi is :", BMI)
+if BMI<=18.5:
+    print("you are underweight")
+elif BMI>18.5  and BMI<24.9:
+    print("you are normal")
+elif BMI>=25 and BMI<=29.9:
+    print("you are overweight")
+else:
+    print("you are obesed")
+
+    # acitivity-3
+num = int(input("Enter a number: "))
+
+if num > 50:
+    print("The number is greater than 50")
+
+    if num % 2 == 0:
+        print("It is even")
+    else:
+        print("It is odd")
+
+else:
+    print("The number is not greater than 50")
+
+    # activity-4
+import datetime
+current_datetime = datetime.datetime.now()
+print("The current_time is :" , current_datetime )
