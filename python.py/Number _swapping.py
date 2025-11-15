e0a3a01252e2c44714576364a552d1592900e2fe
saketh192 <sakethraman964@gmail.com>
2025-11-15 11:17:42 +0530

    NUmber swapping

---
+++ python.py/Number _swapping.py
@@ -0,0 +1,16 @@
+a = int(input("Enter first number (a): "))
+b = int(input("Enter second number (b): "))
+c = int(input("Enter third number (c): "))
+
+print("\nBefore swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
+
+a, b, c = b, c, a
+
+print("\nAfter swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
