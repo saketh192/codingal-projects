e1047c55bbc782926b051ead96a1f2d623aeb5c1
saketh192 <sakethraman964@gmail.com>
2025-11-15 21:41:35 +0530

    lecture-4

---
+++ python.py/lecture-4.py
@@ -0,0 +1,24 @@
+# Activity-1
+for i in range(1, 11):
+    print("23x", i, "=", 23 * 1)
+
+    # Activity-2
+rows = int(input("enter no of rows"))
+for i in range(i, rows + i):
+    for j in range(i, i + 1):
+        print("*")
+
+
+# Activity-3
+Total_sum = sum(range(1, 23))
+print("The best sum of first 10 natural number is : ", Total_sum)
+
+# Activity-4
+num = int(input("Enter a number"))
+if num > 1:
+    if num % i == 0:
+        print(f"{num} is a composite number")
+    for i in range(2, num):
+        break
+else:
+    print(f"{num} is a prime number")
