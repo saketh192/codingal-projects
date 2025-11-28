036ddaebfdf1b0d7ac5558c6783336db5426d7aa
saketh192 <sakethraman964@gmail.com>
2025-11-28 17:01:05 +0530

    turtle

---
+++ python.py/turtle_project.py
@@ -0,0 +1,33 @@
+# The equilateral triangle
+import turtle
+
+
+# t = turtle.Turtle()
+# turtle.bgcolor("green")
+
+# for _ in range(3):
+#     t.forward(100)
+#     t.left(120)
+# turtle.done()
+
+
+# The rectangle
+# rectangle = turtle.Turtle()
+# turtle.bgcolor("lightblue")
+# for i in range(2):
+#     rectangle.forward(120)
+#     rectangle.right(90)
+#     rectangle.forward(60)
+#     rectangle.right(90)
+
+# turtle.done()
+
+# The hexagon
+hexagon = turtle.Turtle()
+turtle.bgcolor("yellow")
+for i in range(6):
+    hexagon.forward(120)
+    hexagon.left(60)
+
+
+turtle.done()
